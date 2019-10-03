b5395733e677056598c6426b4f8bb54ce1f84c88
Matthew Boynes <git@mattboynes.com>
2019-10-03 12:27:51 -0400

    Initial commit

---
+++ remote-backstop.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     Remote Backstop
+ * Plugin URI:      https://github.com/alleyinteractive/remote-backstop
+ * Description:     Safety net for sites depending on remote requests
+ * Author:          Matthew Boynes
+ * Author URI:      https://www.alley.co/
+ * Text Domain:     remote-backstop
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Remote_Backstop
+ */
+
+namespace Remote_Backstop;
+
