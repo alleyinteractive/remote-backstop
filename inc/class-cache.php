93fb11a3f3942a3f7a862ecf3ab15507be6ae477
Matthew Boynes <git@mattboynes.com>
2019-10-03 12:35:17 -0400

    Refactor cache class to be instanced vs static

--- inc/class-cache.php
+++ inc/class-cache.php
@@ -12,10 +12,33 @@ namespace Remote_Backstop;
  */
 class Cache {
 	/**
+	 * Request URL.
+	 *
+	 * @var string
+	 */
+	protected $url;
+
+	/**
+	 * Request arguments.
+	 *
+	 * @var array
+	 */
+	protected $request_args;
+
+	/**
+	 * Cache constructor.
+	 *
+	 * @param string $url          Request URL.
+	 * @param array  $request_args Request args.
+	 */
+	public function __construct( string $url, array $request_args ) {
+		$this->url          = $url;
+		$this->request_args = $request_args;
+	}
+
+	/**
 	 * Get the cache keys storing the "downtime" flags.
 	 *
-	 * @param string $url Request URL.
-	 * @param array  $r   Request args.
 	 * @return array {
 	 *     Cache keys.
 	 *
@@ -24,45 +47,39 @@ class Cache {
 	 *     @type string $request Cache key for the full request.
 	 * }
 	 */
-	public static function down_cache_keys( string $url, array $r ): array {
-		$host = wp_parse_url( $url, PHP_URL_HOST );
+	public function down_cache_keys(): array {
+		$host = wp_parse_url( $this->url, PHP_URL_HOST );
 		return [
 			'host'    => 'rb-down:' . md5( $host ),
-			'url'     => 'rb-down:' . md5( $url ),
-			'request' => 'rb-down:' . self::request_hash( $url, $r ),
+			'url'     => 'rb-down:' . md5( $this->url ),
+			'request' => 'rb-down:' . $this->request_hash(),
 		];
 	}
 
 	/**
 	 * Get the cache key for a request.
 	 *
-	 * @param string $url Request URL.
-	 * @param array  $r   Request args.
 	 * @return string Cache key.
 	 */
-	public static function request_hash( string $url, array $r ): string {
-		return md5( $url . wp_json_encode( $r ) );
+	public function request_hash(): string {
+		return md5( $this->url . wp_json_encode( $this->request_args ) );
 	}
 
 	/**
 	 * Load a request from cache.
 	 *
-	 * @param string $url Request URL.
-	 * @param array  $r   Request args.
 	 * @return bool|array Request array if the request is cached, false if not.
 	 */
-	public static function load_request_from_cache( string $url, array $r ) {
-		return wp_cache_get( self::request_hash( $url, $r ), 'rb-request' );
+	public function load_request_from_cache() {
+		return wp_cache_get( $this->request_hash(), 'rb-request' );
 	}
 
 	/**
 	 * Cache a remote request.
 	 *
 	 * @param \WP_Error|array $response Request response.
-	 * @param string          $url      Request URL.
-	 * @param array           $r        Request args.
 	 */
-	public static function cache_response( $response, string $url, array $r ) {
+	public function cache_response( $response ) {
 		if ( is_wp_error( $response ) ) {
 			$response = [
 				'error'   => true,
@@ -70,18 +87,16 @@ class Cache {
 				'message' => $response->get_error_message(),
 			];
 		}
-		wp_cache_set( self::request_hash( $url, $r ), $response, 'rb-request' );
+		wp_cache_set( $this->request_hash(), $response, 'rb-request' );
 	}
 
 	/**
 	 * Cache downtime.
 	 *
-	 * @param string $url      Request URL.
-	 * @param array  $r        Request args.
-	 * @param int    $duration How long to cache. Defaults to 1 minute.
+	 * @param int $duration How long to cache. Defaults to 1 minute.
 	 */
-	public static function set_down_flag( string $url, array $r, int $duration = MINUTE_IN_SECONDS ) {
-		$cache_keys = self::down_cache_keys( $url, $r );
+	public function set_down_flag( int $duration = MINUTE_IN_SECONDS ) {
+		$cache_keys = $this->down_cache_keys();
 		foreach ( $cache_keys as $cache_key ) {
 			set_transient( $cache_key, 1, $duration );
 		}
@@ -90,15 +105,13 @@ class Cache {
 	/**
 	 * Get the cached flag determining if a resource is down (unavailable).
 	 *
-	 * @param string $url         Request URL.
-	 * @param array  $r           Request args.
 	 * @param string $granularity Optional. How granular should a request check
 	 *                            for downtime. Defaults to 'host', also accepts
 	 *                            'url' or 'request'.
 	 * @return bool True if the resource is down, false if not.
 	 */
-	public static function get_down_flag( string $url, array $r, string $granularity = 'host' ): bool {
-		$cache_keys = self::down_cache_keys( $url, $r );
+	public function get_down_flag( string $granularity = 'host' ): bool {
+		$cache_keys = $this->down_cache_keys();
 		$cache_key  = $cache_keys[ $granularity ] ?? $cache_keys['host'];
 		return (bool) get_transient( $cache_key );
 	}
