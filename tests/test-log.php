85dd52f49a8445301670aea2eddf6a0197eb3bba
Linnea Huxford <7308162+mslinnea@users.noreply.github.com>
2020-02-11 14:52:40 -0800

    Add log tests

--- tests/test-log.php
+++ tests/test-log.php
@@ -16,28 +16,42 @@ use WP_UnitTestCase;
 class LogTest extends WP_UnitTestCase {
 
 	/**
-	 * A single example test.
+	 * Tests that we only have one entry to multiple requests
 	 */
-	public function test_log() {
-		$mock = new Mock_Http_Response();
-		$mock->intercept_next_request()
-		     ->with_body( 'test 1' );
-
-		// Prime the cache.
-		$response = wp_remote_get( 'https://example.com/test-1' );
+	public function test_log_hosts() {
 
 		$mock = new Mock_Http_Response();
-		$mock->intercept_next_request()
-		     ->with_response_code( 500 )
-		     ->with_body( 'error' );
 
-		// Repeat the response, but this time an error is received.
-		$response = wp_remote_get( 'https://example.com/test-1' );
+		// Do 5 requests to the same host in rapid succession; only 1 log entry should exist.
+		for ( $i = 0; $i < 5; $i++ ) {
+			$mock->intercept_next_request()
+			     ->with_response_code( 500 )
+			     ->with_body( 'error' );
 
+			$response = wp_remote_get( 'https://example.com/test-1' );
+		}
 		$log = Log::get_log();
 
+		$this->assertEquals( 1, count( $log ) );
 		$this->assertSame( 'example.com', $log[0]['host'] );
 
+	}
+
+	public function test_log_max() {
+		$mock = new Mock_Http_Response();
+
+		// Do 5 requests to the same host in rapid succession; only 1 log entry should exist.
+		for ( $i = 0; $i < 100; $i++ ) {
+			$mock->intercept_next_request()
+			     ->with_response_code( 500 )
+			     ->with_body( 'error' );
+
+			// Change the host with each request.
+			$response = wp_remote_get( sprintf( 'https://example-%d.com/test', $i ) );
+		}
+		$log = Log::get_log();
+		$this->assertEquals( 50, count( $log ) );
+		$this->assertSame( 'example-99.com', $log[0]['host'] );
 	}
 
 }
